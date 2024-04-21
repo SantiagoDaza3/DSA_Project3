6d0bbdbaf86ccf7b4cf96aae4e77f66f1d757929
Rushilkothur <143026236+Rushilkothur@users.noreply.github.com>
2024-04-20 23:14:37 -0400

    Update Hashfunctions.h

--- Hashfunctions.h
+++ Hashfunctions.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <cstdint>
 #include <string>
+#include "MurmurHash3.h"
 
 uint32_t Hashfunction_fnv1a(const std::string& str);
+uint32_t Hashfunction_mmh3(const std::string& str);
